4cc0d6b9e22b86b278f22f846db0b110f6476e6b
Maksim Ereshov <maksim.ereshov@gmail.com>
2022-07-10 22:15:44 +0300

    Виправив цикл while згідно з зауваженнями щодо введення усякою фігні в строку input() користувачем. Змінив умову "Цікавий вік"

--- Homeworks/HW2/HW2_30-06_Ereshov.py
+++ Homeworks/HW2/HW2_30-06_Ereshov.py
@@ -4,15 +4,22 @@
 while True:
     age = input("Вкажіть ваш вік: ")
     if not age:
-        print("Ви не вказали ваш вік :(")
+        print("Ви не вказали свій вік")
     else:
-        break
+        try:
+            age = int(age)
+        except ValueError:
+            print("Ви можете вказати свій вік лише числами")
+        if isinstance(age, int) and age <= 0:
+            print("Невірно вказан вік")
+        elif isinstance(age, int) and age > 111:
+            print("Невірно вказан вік")
+        if age in range(1, 111, 1):
+            break
 
 # - якщо вік користувача складається з однакових цифр (11, 22, 44 і тд років, всі можливі варіанти!) - вивести "Який цікавий вік!"
-if int(age) > 10 and age[0] == age[-1]:
+if age in range(11, 111, 11): # Я вважаю, що вік 101 та 111 - це теж "цікавий вік"
     print("Який цікавий вік!")
-
-age = int(age)
 
 # - якщо користувачу менше 7 - вивести "Де твої батьки?"
 if age < 7:
